3e6b26fa03b1d9edae83848a1becd9d8a09641f5
vijaybaliah <vijaybalu1989@gmail.com>
2019-10-09 21:26:55 +0530

    feat: added delete functionaliy

--- app.py
+++ app.py
@@ -61,6 +61,19 @@ def set_completed_todo(todo_id):
     return redirect(url_for('index'))
 
 
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except Exception as e:
+        print(e)
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({'success': True})
+
+
 @app.route('/')
 def index():
     return render_template('index.html', data=Todo.query.order_by('id').all())
